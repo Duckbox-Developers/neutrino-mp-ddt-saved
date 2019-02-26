6bd40f5e7a72cf29023cd8abab290f5aac66dcb8
Frankenstone <dampf_acc@yahoo.com>
2019-02-26 08:06:46 +0100

    disable av sync in radio mode
    - thx DbO ;-)

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -4540,6 +4540,9 @@ void CNeutrinoApp::tvMode( bool rezap )
 #ifdef USEACTIONLOG
 	g_ActionLog->println("mode: tv");
 #endif
+
+	videoDecoder->SetSyncMode((AVSYNC_TYPE)g_settings.avsync);
+	audioDecoder->SetSyncMode((AVSYNC_TYPE)g_settings.avsync);
 }
 
 void CNeutrinoApp::scartMode( bool bOnOff )
@@ -4812,6 +4815,9 @@ void CNeutrinoApp::radioMode( bool rezap)
 	if( rezap )
 		channelRezap();
 	frameBuffer->showFrame("radiomode.jpg");
+
+	videoDecoder->SetSyncMode((AVSYNC_TYPE)AVSYNC_DISABLED);
+	audioDecoder->SetSyncMode((AVSYNC_TYPE)AVSYNC_DISABLED);
 }
 
 void CNeutrinoApp::channelRezap()
